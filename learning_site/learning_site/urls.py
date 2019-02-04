e56b326a8e65673c5a8886d8d7f9f0bc5b8171d7
Maureen Oki <maureenoki@Maureens-MacBook-Pro.local>
2019-02-04 11:52:10 +0100

    add inital model, template and view

--- learning_site/learning_site/urls.py
+++ learning_site/learning_site/urls.py
@@ -13,9 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from . import views
+
 
 urlpatterns = [
+    path('courses/', include('courses.url')),
     path('admin/', admin.site.urls),
+    path('', views.index),
 ]
+
+urlpatterns +=  staticfiles_urlpatterns()
